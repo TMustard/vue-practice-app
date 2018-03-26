36796c74050f5e95ffcef264edf090c3c0c55005
Tyler Mustard <tjm.0084@gmail.com>
2018-03-26 12:45:55 -0600

    splash page working

---
+++ src/views/Topics.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="sign-in">
+        <header>
+            <h1>Galvanize Votes</h1>
+
+        </header>
+        <main>
+        <p>This is a topic wrapper</p>
+        <router-view></router-view>
+        </main>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+//    import HelloWorld from "@/components/HelloWorld.vue"
+export default {
+    name: "Topics",
+    components: {
+    }
+}
+</script>
+
+<style scoped lang="scss">
+    @import "~@/styles/_colors";
+    @import "~@/styles/_typography";
+    .topics {
+        @include base-font;
+        header {
+            padding: 1rem;
+            background-color: $dark-color;
+            color: $white;
+            @include-primary-header-font
+            h1 {
+                @include primary-header-font;
+                text-align: center;
+            }
+            img {
+                max-width: 320px;
+                margin-bottom: 2rem;
+            }
+        }
+        main {
+            p {
+                text-align: center;
+            }
+        }
+    }
+</style>
